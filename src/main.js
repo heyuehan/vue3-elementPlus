02fb5c6b371dabb6b56076fc9162077c0317b743
Heyuehan <Heyuehan@rd-acs.com>
2024-07-01 17:12:10 +0800

    第一次提交

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+createApp.use(ElementUI)
+
+createApp(App).use(store).use(router).mount('#app')
